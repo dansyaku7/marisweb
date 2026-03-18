f0f85129c491e3550236c6fa696cced45c18837e
dansyaku7 <syakuronabdan49@gmail.com>
2026-03-19 01:11:22 +0700

    fourth commit

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
-        hostname: "res.cloudinary.com",
         protocol: "https",
-        hostname: '*.public.blob.vercel-storage.com'
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
       },
     ],
-  },
-
-    // Untuk mengabaikan error ESLint
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  // Untuk mengabaikan error TypeScript
-  typescript: {
-    ignoreBuildErrors: true,
   },
 };
 
